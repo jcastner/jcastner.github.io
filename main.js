449e8b6ca514a658ce60c69bd136f6be2ed08c04
James Castner <jcastner@uvm.edu>
2021-04-17 13:59:32 -0400

    Completed homepage animations

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-// Objects
-function Star(x, y, animeIntr, ctx) {
-    this.x = x;
-    this.y = y;
-    this.size = Math.floor((Math.random() * 3) + 1);
-    this.animeIntr = animeIntr;
-    this.draw = function() {
-        ctx.beginPath();
-        ctx.rect(this.x, this.y, this.size, this.size);
-        ctx.fillStyle = "white";
-        ctx.fill();
-    };
-    this.update = function() {
-        this.draw();
-    }
-}
+// Global Variables
+var oldTimeStamp = 0;
+var secondsPassed = 0;
+var launchPadCanvas = document.getElementById("launchPadCanvas");
+var launchPadCanvasContext = launchPadCanvas.getContext("2d");
+var stars = generateStarField();
 
 // Functions
+function Star(x, y) { // Star class
+    this.x = x;
+    this.y = y;
+    this.size = Math.floor((Math.random() * 4) + 2);
+    this.velocity = Math.floor((Math.random() * 10) + 3);
+}
+
+// Generates an array of stars on the screen
 function generateStarField() {
     // Split screen into rows and generate stars from there to avoid over lapping
     var rows = document.body.clientHeight;
@@ -25,47 +23,72 @@ function generateStarField() {
     for (var i = 0; i < rows; i++) {
         for (var j = 0; j < cols; j++) {
             // Draw or naw
-            if(Math.random() > 0.998) {
-                var s = new Star(j, i, ctx);
+            if(Math.random() > 0.9998) {
+                var s = new Star(j, i);
                 stars.push(s);
             }
         }
     }
+
+    return stars;
 }
 
-function renderStarField(stars) {
-    // Generate stars and draw
-    for (var s in stars) {
-        s.draw();
+// Animates the stars
+function updateStarField() {
+    for (var i = 0; i < stars.length; i++) { // Loop through stars and sub y to make moving up animation
+        stars[i].y -= (stars[i].velocity * secondsPassed*2);
+
+        if(stars[i].y < 0) // Remove stars from the array that are not on the screen
+            stars.splice(i, 1);
+
+        if(Math.random() > 0.9999) { // Randomly generate new stars at the bottom of the screen every once in a while
+            var s = new Star(Math.floor((Math.random() * document.body.clientWidth) + 1), document.body.clientHeight);
+            stars.push(s);
+        }
     }
 }
 
-function animateStarField(stars) {
-    
+// Render the stars to the screen
+function renderStarField() {
+    // Generate stars and draw
+    launchPadCanvasContext.clearRect(0, 0, document.body.clientWidth, document.body.clientHeight); // Clear the screen
+    launchPadCanvasContext.fillStyle = "white";
+    for (var i = 0; i < stars.length; i++) { // Draw all stars
+        launchPadCanvasContext.beginPath();
+        launchPadCanvasContext.rect(stars[i].x, stars[i].y, stars[i].size, stars[i].size);
+        launchPadCanvasContext.fill();
+    }
 }
 
-function init() {
-    timer = setInterval(animateStarField, 5);
-    return timer;
+// Animation loop for moving stars
+function animationLoop(timestamp) {
+    secondsPassed = (timestamp - oldTimeStamp) / 1000;
+    oldTimeStamp = timestamp;
+
+    updateStarField(secondsPassed);
+    renderStarField();
+
+    var fps = Math.round(1/secondsPassed);
+    launchPadCanvasContext.fillText("FPS: " + fps, 10, 30);
+
+    window.requestAnimationFrame(animationLoop);
 }
 
-$(function() {
-    // Initialize variables
-    var launchPadCanvas = document.getElementById("launchPadCanvas");
-    var launchPadCanvasContext = launchPadCanvas.getContext("2d");
-
+$(function() { // Page load
     // Initial setup stuff
     launchPadCanvas.width = document.body.clientWidth;
     launchPadCanvas.height = document.body.clientHeight;
     $('#welcomeBanner').css('opacity', '0.0').animate({'opacity': '.96'}, 5000);
 
-    // Render the starfield to the screen
-    renderStarField(launchPadCanvasContext);
+    window.requestAnimationFrame(animationLoop);
 
     // Window resizing things
     $(window).on('resize', function() {
         launchPadCanvas.width = document.body.clientWidth;
         launchPadCanvas.height = document.body.clientHeight;
-        renderStarField(launchPadCanvasContext);
+
+        // stars = generateStarField(launchPadCanvasContext);
+        // stopAnimation();
+        // startAnimation();
     });
 });
